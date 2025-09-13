7d51229a23f93d7db51e63749aefbbbbc481f06b
Angela Kormushoska <a_kormusho18@cs.uni-kl.de>
2025-09-13 18:31:44 +0200

    Implement authentication and configuration

---
+++ lab/authentication.py
@@ -0,0 +1,26 @@
+from msal import PublicClientApplication
+from config import Config
+
+app = PublicClientApplication(client_id=Config.CLIENT_ID, authority=Config.AUTHORITY)
+
+result = None
+accounts = app.get_accounts()
+if accounts:
+    print("Pick the account you want to use to proceed:")
+    for a in accounts:
+        print(a["username"])
+    chosen = accounts[0]
+    result = app.acquire_token_silent(["User.Read"], account=chosen)
+
+if not result:
+    print("No suitable token in cache found, getting a new one from Azure AD.")
+    result = app.acquire_token_interactive(scopes=["User.Read"], prompt="select_account")
+
+if "access_token" in result:
+    print("Authentication success. This is your token: ")
+    print(result["access_token"])
+else:
+    print["Authentication failed."]
+    print(result.get("error"))
+    print(result.get("error_description"))
+    print(result.get("correlation_id"))
