1900a054f133ae43ab2a698b6e6cdcd33d4d4861
Angela Kormushoska <a_kormusho18@cs.uni-kl.de>
2025-09-15 19:52:36 +0200

    Implement hash algorithm and compare files

--- lab/main.py
+++ lab/main.py
@@ -1,11 +1,20 @@
-from lab import authentication, upload, download
+from lab import authentication, upload, download, integrity_verification
 from config import Config
+import os
 
 def main():
     access_token = authentication.get_token()
     upload.create_folder(access_token, Config.UPLOAD_FOLDER)
     upload.upload_file(access_token, Config.UPLOAD_FOLDER, Config.LOCAL_FILE_PATH)
     download.download_file(access_token, Config.UPLOAD_FOLDER, Config.LOCAL_FILE_PATH, Config.DOWNLOAD_FOLDER_PATH)
+
+    original_hash = integrity_verification.sha256_hash(Config.LOCAL_FILE_PATH)
+    download_hash = integrity_verification.sha256_hash(f"{Config.DOWNLOAD_FOLDER_PATH}/{os.path.basename(Config.LOCAL_FILE_PATH)}")
+
+    if original_hash == download_hash:
+        print("Hashes match.")
+    else:
+        print("Hashes do not match.")
 
 if __name__ == "__main__":
     main()
