389f293f1243bff5125bacfc589d37617b3499d1
Angela Kormushoska <a_kormusho18@cs.uni-kl.de>
2025-09-15 04:03:23 +0200

    Implement upload file function

--- lab/upload.py
+++ lab/upload.py
@@ -1,7 +1,6 @@
 from config import Config
 import requests
-from lab import *
-
+import os
 
 def create_folder(access_token, folder_name):
     url = f"{Config.GRAPH_API}/{Config.VERSION}/{Config.RESOURCE}"
@@ -17,11 +16,25 @@ def create_folder(access_token, folder_name):
         "@microsoft.graph.conflictBehavior": "rename"
     }
 
-    response = requests.post(url, headers=headers, json=data)
+    response = requests.post(url, headers=headers, json=folder_data)
 
     if response.status_code in [200, 201]:
         print("Folder created successfully.")
     else:
         print("Failed to create folder.")
 
+def upload_file(access_token, folder_name, file_path):
+    file_name = os.path.basename(file_path)
+    url = f"{Config.GRAPH_API}/{Config.VERSION}/me/drive/root:/{folder_name}/{file_name}:/content"
 
+    headers = {
+        "Authorization": f"Bearer {access_token}",
+        "Content-Type": "application/json"
+    }
+
+    response = requests.put(url, headers=headers, data=open(file_path, "rb"))
+
+    if response.status_code in [200, 201]:
+        print("File uploaded successfully.")
+    else:
+        print("Failed to upload file.")
