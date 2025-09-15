587f71d60f98c17f8ee3f5e24a4b03cecf4c3727
Angela Kormushoska <a_kormusho18@cs.uni-kl.de>
2025-09-15 18:49:54 +0200

    Implement download function

--- lab/config.py
+++ lab/config.py
@@ -12,3 +12,4 @@ class Config:
     RESOURCE = os.getenv("RESOURCE")
     UPLOAD_FOLDER = os.getenv("UPLOAD_FOLDER")
     LOCAL_FILE_PATH = os.getenv("LOCAL_FILE_PATH")
+    DOWNLOAD_FOLDER_PATH = os.getenv("DOWNLOAD_FOLDER_PATH")
