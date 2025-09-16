8b6291f9592694404a45c833a5644283cb3ba433
Angela Kormushoska <a_kormusho18@cs.uni-kl.de>
2025-09-16 11:25:51 +0200

    Fix create folder logic

--- lab/upload.py
+++ lab/upload.py
@@ -2,27 +2,6 @@ from config import Config
 import requests
 import os
 
-def create_folder(access_token, folder_name):
-    url = f"{Config.GRAPH_API}/{Config.VERSION}/{Config.RESOURCE}/root/children"
-
-    headers = {
-        "Authorization": f"Bearer {access_token}",
-        "Content-Type": "application/json"
-    }
-
-    folder_data = {
-        "name": folder_name,
-        "folder": {},
-        "@microsoft.graph.conflictBehavior": "rename"
-    }
-
-    response = requests.post(url, headers=headers, json=folder_data)
-
-    if response.status_code in [200, 201]:
-        print("Folder created successfully.")
-    else:
-        print("Failed to create folder.")
-
 def upload_file(access_token, folder_name, file_path):
     file_name = os.path.basename(file_path)
     url = f"{Config.GRAPH_API}/{Config.VERSION}/{Config.RESOURCE}/root:/{folder_name}/{file_name}:/content"
